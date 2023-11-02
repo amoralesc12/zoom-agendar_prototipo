3858f8f577a5d53db89d16b6cb43aebef59ca7cb
Alejandra Morales <61769099+amoralesc12@users.noreply.github.com>
2023-11-01 23:27:19 -0600

    solo subi al repo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div style={{ display: "flex", flexDirection: "col"}}>
       <Navbar />
-      <MyZoomPat/>
+      <Scheduler/>
       {/*<MyZoomPat/>
       /*<Scheduler/>*/}
     </div>
